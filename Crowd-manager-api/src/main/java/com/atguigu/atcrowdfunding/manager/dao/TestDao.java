5118af7455f6d5935bc3735860977a3c3dba104c
panjinhome <com.panjin@foxmail.com>
2019-02-27 18:06:27 +0800

    框架测试（出现bug，未解决）

---
+++ Crowd-manager-api/src/main/java/com/atguigu/atcrowdfunding/manager/dao/TestDao.java
@@ -0,0 +1,11 @@
+package com.atguigu.atcrowdfunding.manager.dao;
+
+import java.util.Map;
+
+/**
+ * @author panjin
+ * @date 2019/2/2715:36
+ */
+public interface TestDao {
+    public void insert(Map map);
+}
